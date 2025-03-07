2855ed55a01c7f65d4f1c7473b7802338f90530c
Pal Kerecsenyi <pal@p.kerecs.com>
2025-03-07 00:10:02 +0000

    More memory efficient measurement

--- src/speedtest.rs
+++ src/speedtest.rs
@@ -1,16 +1,19 @@
+use futures_util::stream::StreamExt;
 use std::time::Instant;
 
 pub async fn speedtest() -> f64 {
     let now = Instant::now();
-    let bytes = reqwest::get("https://speedtest.palk.dev/bigfile.bin")
+    let mut bytes = reqwest::get("https://speedtest.palk.dev/bigfile.bin")
         .await
         .expect("Random data to load.")
-        .bytes()
-        .await
-        .expect("Bytes to parse.");
+        .bytes_stream();
+
+    let mut num_bytes = 0_f64;
+    while let Some(b) = bytes.next().await {
+        let b = b.unwrap();
+        num_bytes += b.len() as f64;
+    }
+
     let elapsed = now.elapsed();
-
-    let num_bytes = bytes.len() as f64;
-
     num_bytes / elapsed.as_secs_f64()
 }
